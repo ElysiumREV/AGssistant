ea20c167443c24046bacc4d7138f905b22c4842b
ely <justomelone@gmail.com>
2024-10-05 18:31:12 -0300

    Update 05/10/2024 layout

--- AGssistant/Main.Designer.cs
+++ AGssistant/Main.Designer.cs
@@ -29,16 +29,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             panel1 = new Panel();
+            button1 = new Button();
+            label1 = new Label();
             button6 = new Button();
             title = new Label();
-            button1 = new Button();
             panelContainer = new Panel();
             panel3 = new Panel();
+            label2 = new Label();
             button2 = new Button();
             button5 = new Button();
             button3 = new Button();
             button4 = new Button();
+            label3 = new Label();
             panel1.SuspendLayout();
             panelContainer.SuspendLayout();
             panel3.SuspendLayout();
@@ -47,58 +51,78 @@
             // panel1
             // 
             panel1.BackColor = Color.Black;
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label1);
             panel1.Controls.Add(button6);
             panel1.Controls.Add(title);
-            panel1.Controls.Add(button1);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(784, 60);
+            panel1.Size = new Size(784, 86);
             panel1.TabIndex = 1;
             panel1.Paint += panel1_Paint;
             // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 62);
+            button1.TabIndex = 6;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Fira Sans", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Transparent;
+            label1.Location = new Point(370, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 23);
+            label1.TabIndex = 5;
+            label1.Text = "label1";
+            // 
             // button6
             // 
-            button6.Location = new Point(697, 12);
+            button6.Dock = DockStyle.Right;
+            button6.Location = new Point(709, 0);
             button6.Name = "button6";
-            button6.Size = new Size(75, 30);
+            button6.Size = new Size(75, 86);
             button6.TabIndex = 4;
             button6.Text = "Sobre";
             button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click_1;
             // 
             // title
             // 
             title.Anchor = AnchorStyles.Top;
             title.AutoSize = true;
-            title.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            title.Font = new Font("Fira Sans", 27.7499962F, FontStyle.Regular, GraphicsUnit.Point, 0);
             title.ForeColor = Color.White;
-            title.Location = new Point(302, 0);
+            title.Location = new Point(304, 10);
             title.Name = "title";
-            title.Size = new Size(193, 50);
+            title.Size = new Size(193, 45);
             title.TabIndex = 0;
             title.Text = "AGssistant";
             title.Click += title_Click;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(12, 12);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 30);
-            button1.TabIndex = 0;
-            button1.Text = "Home";
-            button1.UseVisualStyleBackColor = true;
             // 
             // panelContainer
             // 
             panelContainer.Controls.Add(panel3);
             panelContainer.Dock = DockStyle.Fill;
-            panelContainer.Location = new Point(0, 60);
+            panelContainer.Location = new Point(0, 86);
             panelContainer.Name = "panelContainer";
-            panelContainer.Size = new Size(784, 381);
+            panelContainer.Size = new Size(784, 355);
             panelContainer.TabIndex = 3;
             // 
             // panel3
             // 
+            panel3.BackColor = Color.Transparent;
+            panel3.Controls.Add(label3);
+            panel3.Controls.Add(label2);
             panel3.Controls.Add(button2);
             panel3.Controls.Add(button5);
             panel3.Controls.Add(button3);
@@ -106,26 +130,36 @@
             panel3.Dock = DockStyle.Fill;
             panel3.Location = new Point(0, 0);
             panel3.Name = "panel3";
-            panel3.Size = new Size(784, 381);
+            panel3.Size = new Size(784, 355);
             panel3.TabIndex = 4;
-            panel3.Paint += panel3_Paint;   
+            panel3.Paint += panel3_Paint;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Fira Sans", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(582, 45);
+            label2.Name = "label2";
+            label2.Size = new Size(190, 40);
+            label2.TabIndex = 4;
+            label2.Text = "Nossas Dicas!";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            label2.Click += label2_Click;
             // 
             // button2
             // 
-            button2.Anchor = AnchorStyles.Right;
-            button2.Location = new Point(697, 32);
+            button2.Location = new Point(582, 95);
             button2.Name = "button2";
-            button2.Size = new Size(75, 30);
+            button2.Size = new Size(190, 30);
             button2.TabIndex = 1;
-            button2.Text = "Dica 1";
+            button2.Text = "Para o Corpo";
             button2.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
             button5.Anchor = AnchorStyles.Right;
-            button5.Location = new Point(697, 140);
+            button5.Location = new Point(582, 203);
             button5.Name = "button5";
-            button5.Size = new Size(75, 30);
+            button5.Size = new Size(190, 30);
             button5.TabIndex = 3;
             button5.Text = "Mais Dicas";
             button5.UseVisualStyleBackColor = true;
@@ -134,22 +168,30 @@
             // button3
             // 
             button3.Anchor = AnchorStyles.Right;
-            button3.Location = new Point(697, 68);
+            button3.Location = new Point(582, 131);
             button3.Name = "button3";
-            button3.Size = new Size(75, 30);
+            button3.Size = new Size(190, 30);
             button3.TabIndex = 2;
-            button3.Text = "Dica 2";
+            button3.Text = "Para a Mente";
             button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
             button4.Anchor = AnchorStyles.Right;
-            button4.Location = new Point(697, 104);
+            button4.Location = new Point(582, 167);
             button4.Name = "button4";
-            button4.Size = new Size(75, 30);
+            button4.Size = new Size(190, 30);
             button4.TabIndex = 3;
-            button4.Text = "Dica 3";
+            button4.Text = "Para a Segurança";
             button4.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.Image = (Image)resources.GetObject("label3.Image");
+            label3.Location = new Point(12, 6);
+            label3.Name = "label3";
+            label3.Size = new Size(230, 340);
+            label3.TabIndex = 5;
             // 
             // Main
             // 
@@ -176,10 +218,13 @@
         private Panel panel3;
         private Panel drawstring;
         private Button button6;
-        private Button button1;
         private Button button2;
         private Button button5;
         private Button button3;
         private Button button4;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Label label3;
     }
 }
--- AGssistant/Main.cs
+++ AGssistant/Main.cs
@@ -9,10 +9,6 @@ namespace AGssistant
         public Main()
         {
             InitializeComponent();
-
-            string fundoAmarelo = "#FBFFCD";
-            Color myColor = System.Drawing.ColorTranslator.FromHtml(fundoAmarelo);
-            panel3.BackColor = myColor;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -48,12 +44,16 @@ namespace AGssistant
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            
+            string fundoAzul = "#1C5560";
+            Color myColor = System.Drawing.ColorTranslator.FromHtml(fundoAzul);
+            panel1.BackColor = myColor;
         }
 
         private void panel3_Paint(object sender, PaintEventArgs e)
         {
-         
+            string fundoAmarelo = "#FBFFCD";
+            Color myColor = System.Drawing.ColorTranslator.FromHtml(fundoAmarelo);
+            panel3.BackColor = myColor;
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -69,6 +69,32 @@ namespace AGssistant
         private void title_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            string fundoAzul = "#1C5560";
+            Color myColor = System.Drawing.ColorTranslator.FromHtml(fundoAzul);
+            label2.BackColor = myColor;
+        }
+
+        private void button1_Click_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_4(object sender, EventArgs e)
+        {
+            string fundoAzul = "#1C5560";
+            Color myColor = System.Drawing.ColorTranslator.FromHtml(fundoAzul);
+            button1.BackColor = myColor;
+
+            
         }
     }
 }
